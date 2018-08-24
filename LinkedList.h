150ae5a39f27a0190b9e911b6a5469824de3df87
William Keely <williamkeely@gmail.com>
2018-08-24 12:20:37 -0500

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -2,33 +2,22 @@
 // Created by willi on 6/24/2018.
 //
 
+
 #ifndef MYDATASTRUCTS_LINKEDLIST_H
 #define MYDATASTRUCTS_LINKEDLIST_H
 
-template<class T>
-class Node{
-    friend class LinkedList;
-protected:
-    T *element;
-    Node *nextNode;
 
-public:
-    Node(T *element);
-    ~Node();
-};
-template<class T>
-Node<T>::Node(T *element) {
-    this->element = element;
-    nextNode = nullptr;
-}
 
 
 
 template<class T>
 class LinkedList{
+    struct Node{
+        T *element;
+        Node *nextNode;
+    };
 protected:
-    Node<T> *headNode;
-    Node<T> *tailNode;
+    Node *headNode;
     unsigned int length;
 
 public:
@@ -36,38 +25,95 @@ public:
     ~LinkedList();
 
     void add(T e);
+    void remove(T e);
+    void reverse();
 
 };
 template<class T>
 LinkedList<T>::LinkedList() {
     headNode = nullptr;
-    tailNode = nullptr;
     length = 0;
 
 
 }
 template<class T>
 LinkedList<T>::~LinkedList() {
-    for(int i = 0; i < length; i++){
-        delete headNode;
+    Node *temp, *next;
+    temp = headNode;
+    if(temp == nullptr){
+        return;
+    }
+
+    while(temp){
+        next = temp->nextNode;
+        delete temp;
+        temp = next;
     }
 }
 template<class T>
 void LinkedList<T>::add(T e) {
-    Node *temp = new Node(e);
-    temp->element = e;
-    temp->nextNode = nullptr;
+    Node *last, *temp;
+
 
     if(headNode == nullptr){
-        headNode = temp;
-        tailNode = temp;
+        headNode = new Node;
+        headNode->element = e;
+        headNode->nextNode = nullptr;
+
         length++;
+        return;
     }
-    else{
-        tailNode->nextNode = temp;
-        tailNode = tailNode->nextNode;
+    last= headNode;
+    while(last->nextNode != nullptr){
+        last = last->nextNode;
+    }
+    temp = new Node;
+    temp->element = e;
+    temp->nextNode = nullptr;
+    last->nextNode = temp;
+
+}
+template <typename T>
+void LinkedList<T>::remove(T e) {
+    Node *last, *temp;
+    temp = headNode;
+    if(temp == nullptr){
+        return;
+    }
+    if(temp->nextNode == nullptr){
+        delete temp;
+    }
+    while(temp){
+        last = temp;
+        temp = temp->nextNode;
+        if(temp->nextNode == nullptr){
+            delete last;
+            temp = nullptr;
+        }
+
     }
 
+}
+template <typename T>
+void LinkedList<T>::reverse() {
+    if(headNode == nullptr || headNode->nextNode == nullptr){
+        return;
+    }
+    Node *start = headNode;
+    Node *curr = headNode->nextNode;
+    Node *next = curr->nextNode;
+
+    headNode->nextNode = nullptr;
+    curr->nextNode = headNode;
+
+    while(next != nullptr){
+        curr->nextNode = start;
+        start = curr;
+        curr = next;
+        next = next->nextNode;
+    }
+    headNode = curr;
+    headNode->nextNode = start;
 }
 
 #endif //MYDATASTRUCTS_LINKEDLIST_H
